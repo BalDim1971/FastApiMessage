5de260cbd30e5024193dcfa1913b1535f33f0884
BalashovDV <korochun@yandex.ru>
2024-08-18 07:22:43 +0300

    Простое приложение на FastAPI для обмена сообщениями. 1. Основной скелет. 2. Запускается с выдачей сообщения: "Приложение работает". 3. Моделей и роутеров пока нет.

---
+++ main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI, HTTPException, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Приложение работает!"}
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"message": exc.detail},
+    )
+
+
+if __name__ == '__main__':
+    import uvicorn
+    
+    uvicorn.run(
+        "main:app",
+        host="127.0.0.1",
+        port=8000,
+        log_level="info",
+        reload=True)
